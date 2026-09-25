fe563422820b31da28fa1495d1850fd2ab34e6c1
abrshz <trusewynur@gmail.com>
2026-09-25 10:28:29 +0300

    Variales class done

---
+++ JavaScript/Chapter 1/dataTypes.js
@@ -0,0 +1,63 @@
+// Variable (ክንዲ ስም)፥-  is a named storage for data. Variables can be declared using var, let, or const keywords. var name = "Alice"; let age = 25; const isStudent = true;
+
+// ንስኺ = ኣቢቲ
+// ንስኻ = ሆሲ
+// ኣነ = ኣብርሃም
+
+// Var old version of declaring variables, function-scoped, can be redeclared and updated.
+
+// Let is a newer version of declaring variables, block-scoped, can be updated but not redeclared within the same scope.
+
+// Const is used to declare variables that cannot be reassigned, block-scoped, must be initialized at the time of declaration.
+
+// Data types are the different types of values that can be stored and manipulated in a programming language. In JavaScript, there are several data types, including:
+
+// String is a sequence of characters enclosed in single ' ' or double quotes " ". // text
+
+let nameStudent = "John Doe"; // String double quotes
+
+// console.log(nameStudent);
+// console.log(typeof nameStudent);
+
+let country = "Ethiopia"; // String single quotes
+
+// console.log(country);
+// console.log(typeof country);
+
+// Numbers are numeric values that can be integers or floating-point numbers. // numbers
+
+let ageStudent = 20; // Integer
+
+// console.log(ageStudent);
+// console.log(typeof ageStudent);
+
+let height = 1.79; // Floating-point number
+//height = 1.82; // Updating the value of height
+
+console.log(height);
+console.log(typeof height);
+
+const pi = 3.14; // Floating-point number
+
+// console.log(pi);
+// console.log(typeof pi);
+
+// Boolean represents a logical entity and can have two values: true or false. // true or false
+
+let isStudent = true; // Boolean true
+
+// console.log(isStudent);
+// console.log(typeof isStudent);
+
+let isCompleted = false; // Boolean false
+
+// console.log(isCompleted);
+// console.log(typeof isCompleted);
+
+// Null is a special value that represents the absence of any object value.
+
+// Undefined is a primitive value automatically assigned to variables that have just been declared, or to formal arguments for which there are no actual arguments.
+
+// Array is a special type of object used for storing multiple values in a single variable. Arrays are created using square brackets [] and can hold values of different data types. [1, 2, 3, "four", true]
+
+// Object is a collection of properties, where each property is defined as a key-value pair. Objects are created using curly braces {}. { name: "John", age: 30, isStudent: true }
